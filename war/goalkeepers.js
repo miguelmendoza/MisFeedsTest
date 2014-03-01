67575629c825ad497952f5d597084724d7a1ee77
Miguel Mendoza <miguel.mendoza@esmas.net>
2014-02-28 18:27:13 -0600

    Inicializacion

---
+++ war/goalkeepers.js
@@ -0,0 +1,54 @@
+stdisticPorteros({
+    "idTorneo": 234,
+    "nameTorneo": "Liga Bancomer MX Clausura 2014",
+    "nameWebTorneo": "liga-mx-clausura-2014",
+    "idJornada": 15,
+    "nameJornada": "Jornada 5",
+    "dataEstadistica": [
+        {
+            "no": 1,
+            "idPlayer": 456,
+            "namePlayer": "Rodrigo Marquez",
+            "teamPlayer": {
+                "idTeam": 27,
+                "nombreTeam": "Toluca",
+                "aliasTeam": "TOL"
+            },
+            "estadisticasPlayer": {
+                "MINUTOS": 15,
+                "GOLES": 32,
+                "PROMEDIO": 32
+            }
+        },
+        {
+            "no": 1,
+            "idPlayer": 456,
+            "namePlayer": "Rodrigo Marquez",
+            "teamPlayer": {
+                "idTeam": 27,
+                "nombreTeam": "Toluca",
+                "aliasTeam": "TOL"
+            },
+            "estadisticasPlayer": {
+                "MINUTOS": 15,
+                "GOLES": 32,
+                "PROMEDIO": 32
+            }
+        },
+        {
+            "no": 1,
+            "idPlayer": 456,
+            "namePlayer": "Rodrigo Marquez",
+            "teamPlayer": {
+                "idTeam": 27,
+                "nombreTeam": "Toluca",
+                "aliasTeam": "TOL"
+            },
+            "estadisticasPlayer": {
+                "MINUTOS": 15,
+                "GOLES": 32,
+                "PROMEDIO": 32
+            }
+        }
+	]
+});
---
+++ war/phases.js
@@ -0,0 +1,120 @@
+phasesbytorneo({
+    "idTorneo": 361,
+    "nameTorneo": "Torneo MX Clausura 2014",
+    "nameWebTorneo": "torneo-mx-clausura-2014",
+    "dataFases": [
+        {
+            "nombreFase": "Fase 2",
+            "idFase": 13,
+            "dataJornadas": [
+                {
+                    "idTeam": 8,
+                    "group": "A",
+                    "positionTeam": 1
+                }, 
+                {
+                    "idTeam": 5,
+                    "group": "A",
+                    "positionTeam": 2
+                },
+                {
+                    "idTeam": 6,
+                    "group": "A",
+                    "positionTeam": 3
+                },
+                {
+                    "idTeam": 7,
+                    "group": "A",
+                    "positionTeam": 3
+                },
+                {
+                    "idTeam": 9,
+                    "group": "B",
+                    "positionTeam": 3
+                },
+                {
+                    "idTeam": 10,
+                    "group": "B",
+                    "positionTeam": 3
+                },
+                {
+                    "idTeam": 11,
+                    "group": "B",
+                    "positionTeam": 3
+                },
+                {
+                    "idTeam": 12,
+                    "group": "B",
+                    "positionTeam": 3
+                },
+                {
+                    "idTeam": 13,
+                    "group": "C",
+                    "positionTeam": 3
+                },
+                {
+                    "idTeam": 14,
+                    "group": "C",
+                    "positionTeam": 3
+                },
+                {
+                    "idTeam": 15,
+                    "group": "D",
+                    "positionTeam": 3
+                },
+                {
+                    "idTeam": 15,
+                    "group": "D",
+                    "positionTeam": 3
+                },
+                {
+                    "idTeam": 15,
+                    "group": "D",
+                    "positionTeam": 3
+                },
+                {
+                    "idTeam": 15,
+                    "group": "D",
+                    "positionTeam": 3
+                }
+             
+            ]
+        },
+        {
+            "nombreFase": "Fase 3",
+            "idFase": 12,
+            "dataJornadas": [
+                {
+                    "idTeam": 7,
+                    "group": "1",
+                    "positionTeam": 1
+                }, 
+                {
+                    "idTeam": 8,
+                    "group": "2",
+                    "positionTeam": 2
+                },
+                {
+                    "idTeam": 10,
+                    "group": "3",
+                    "positionTeam": 3
+                },
+                {
+                    "idTeam": 6,
+                    "group": "1",
+                    "positionTeam": 1
+                }, 
+                {
+                    "idTeam": 9,
+                    "group": "2",
+                    "positionTeam": 2
+                },
+                {
+                    "idTeam": 11,
+                    "group": "3",
+                    "positionTeam": 3
+                }
+            ]
+        }
+    ]
+});
