dc8493900d55d2c159efb1cd66dbeaec6bdb6213
Miguel Mendoza <miguel.mendoza@esmas.net>
2014-03-07 18:07:31 -0600

    Widget Terminados

---
+++ war/data/coach.js
@@ -0,0 +1,44 @@
+stadisticDT({
+    "idDT": 22,
+    "webNameDT": "ricardo-antonio-mohamed",
+    "nameDT": "Antonio Ricardo Mohamed Matijevich",
+    "urlPhotoPlayer": "http://i2.esmas.com/sef/img/coach/photoprofile/antonio-mohamed-300-350.jpg",
+    "dataTecnico": {
+        "fullName": "Antonio Ricardo Mohamed Matijevich",
+        "posicion": "Director T&eacute;cnico",
+        "fechaNacimiento": "02/04/1970",
+        "lugarNacimiento": "Buenos Aires, Argentina",
+        "nacionalidad": "Mexicana",
+        "trayectoria": "<p>Comenz&oacute; su carrera de técnico con el Zacatepec en la Primera A, también dirigió a Querétaro en esa división.</p><p>En Pimera estuvo al frente de Morelia (Clausura 2004), Jaguares (Clausura 2005) y Veracruz (Apertura 2007 y Clausura 2008).</p><p>En Argentina ha estado al frente del Huracán de Primera B  Nacional al que ascendió, del Atlético Colón de Santa Fe y del  Independiente.</p><p>Regresó a México para dirigir a Xolos en el Apertura 2011 y fue campeón en el Apertura 2012.</p><p>Se fue a su país para dirigir a Huracán, pero regresó a México, con América a partir del Clausura 2014.</p>"
+    },
+    "estadisticasTecnico": [
+        {
+          "idTorneo": "234",
+            "nameTorneo": "Liga MX Clausura 2014",
+            "idClub": "34",
+            "nameClub": "Am&eacute;rica",
+            "JJ": "0",
+            "JG": "0",
+            "JE": "0",
+            "JP": "0",
+            "GF": "0",
+            "GC": "0",
+            "TR": "0",
+            "PTS": "0"
+        },
+        {
+            "idTorneo": "234",
+            "nameTorneo": "Copa Libertadores 2013",
+            "idClub": "35",
+            "nameClub": "Tijuana",
+            "JJ": "8",
+            "JG": "5",
+            "JE": "2",
+            "JP": "1",
+            "GF": "10",
+            "GC": "5",
+            "TR": "0",
+            "PTS": "17"
+        }
+    ]
+});
